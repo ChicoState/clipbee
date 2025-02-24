01b0726e1e012d6bc71626b881b777b5d4b49747
Kamrin Bains <kamrinbains@gmail.com>
2025-02-24 13:16:56 -0800

    Added tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { viteStaticCopy } from 'vite-plugin-static-copy';
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     react(),
     viteStaticCopy({
       targets: [
